11c24c9ee4041ab806c1638e64d6e2f648cb8d7c
Craverman <CraVerka@googlemail.com>
2022-11-29 16:25:01 +0300

    creating db generator and fixing models

--- seoproject/mainapp/models.py
+++ seoproject/mainapp/models.py
@@ -3,57 +3,81 @@ from django.urls import reverse
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Название")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
-    description = models.TextField(blank=True, verbose_name="Описание")
-    telephone = models.CharField(max_length=16, verbose_name="Телефон")
-    address = models.TextField(blank=True, verbose_name="Адрес")
-    nearest_metro_stations = models.TextField(blank=True, verbose_name="Ближайшая станция метро")
-    opening_hours = models.TextField(blank=True, verbose_name="Часы работы")
-    services = models.TextField(blank=True, verbose_name="Сервис")
-    review = models.ImageField(blank=True, verbose_name="Отзывы")
-    rating = models.ImageField(blank=True, verbose_name="Рейтинг")
-    stars = models.ImageField(blank=True, verbose_name="Звезды")
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
-    time_create = models.DateField(auto_now_add=True, verbose_name="Время создания")
-    time_update = models.DateField(auto_now=True, verbose_name="Время изменения")
-    # revs = models.ForeignKey('Reviews', on_delete=models.PROTECT, verbose_name="Отзывы")
-
-    def __str__(self):
-        return self.name  # возвращает заголовок текущей записи
-
-    def get_absolute_url(self):
-        """
-        Формирует абсолютный url для шаблона
-        :return:
-        """
-        return reverse('company', kwargs={'slug': self.slug})
+    name = models.CharField(
+        verbose_name="Наименование компании",
+        max_length=64,
+        unique=True,
+        blank=True
+    )
+    description = models.TextField(
+        verbose_name="описание",
+        blank=True)
+    services = models.TextField(
+        blank=True,
+        verbose_name="Оказываемые услуги"
+    )
+    rating = models.IntegerField(
+        verbose_name="Цифра рейтинга",
+        blank=True
+    )
+    stars = models.IntegerField(
+        verbose_name="Кол-во звезд от 1-5",
+        blank=True
+    )
+    photo = models.ImageField(
+        upload_to="photos/%Y/%m/%d/",
+        verbose_name="Фото"
+    )
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+        verbose_name="URL"
+    )
+    time_create = models.DateField(
+        auto_now_add=True,
+        verbose_name="Время создания"
+    )
 
     class Meta:
         verbose_name = 'Компания'
         verbose_name_plural = 'Компании'
-        ordering = ['time_create', 'name']
-
-
-class Reviews(models.Model):
-    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
-    description = models.TextField(blank=True, verbose_name="Описание")
-    stars = models.ImageField(blank=True, verbose_name="Звезды")
-    email = models.EmailField(max_length=100, unique=True, verbose_name="Email")
-    company = models.ForeignKey('Company', on_delete=models.PROTECT, verbose_name="Компания")
+        ordering = ['id', 'name']
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        """
-        Формирует абсолютный url для шаблона
-        :return:
-        """
-        return reverse('reviews', kwargs={'slug': self.slug})
+
+class Reviews(models.Model):
+    name = models.CharField(
+        verbose_name="ФИО",
+        max_length=64,
+        unique=True,
+        blank=True
+    )
+    description = models.TextField(
+        verbose_name="описание",
+        blank=True
+    )
+    Company = models.ForeignKey(
+        "Company",
+        related_name="+",
+        on_delete=models.CASCADE
+    )
+    stars = models.IntegerField(
+        verbose_name="Кол-во звезд от 1-5",
+        blank=True
+    )
+    email = models.EmailField(
+        max_length=254,
+        verbose_name="электронная почта"
+    )
 
     class Meta:
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
         ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
