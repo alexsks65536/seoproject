87fca863f121d170a958145a9477baaa136514ba
alexsks65536 <megbyte@mail.ru>
2022-11-29 14:00:39 +0300

    доработка шаблона

--- seoproject/mainapp/models.py
+++ seoproject/mainapp/models.py
@@ -3,34 +3,45 @@ from django.urls import reverse
 
 
 class Company(models.Model):
-    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    name = models.CharField(max_length=255, verbose_name="Название")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
-    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    telephone = models.CharField(max_length=16, verbose_name="Телефон")
+    address = models.TextField(blank=True, verbose_name="Адрес")
+    nearest_metro_stations = models.TextField(blank=True, verbose_name="Ближайшая станция метро")
+    opening_hours = models.TextField(blank=True, verbose_name="Часы работы")
+    services = models.TextField(blank=True, verbose_name="Сервис")
+    review = models.ImageField(blank=True, verbose_name="Отзывы")
+    rating = models.ImageField(blank=True, verbose_name="Рейтинг")
+    stars = models.ImageField(blank=True, verbose_name="Звезды")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateField(auto_now=True, verbose_name="Время изменения")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
-    prod = models.ForeignKey('Product', on_delete=models.PROTECT, verbose_name="Продукт")
+    # revs = models.ForeignKey('Reviews', on_delete=models.PROTECT, verbose_name="Отзывы")
 
     def __str__(self):
-        return self.title  # возвращает заголовок текущей записи
+        return self.name  # возвращает заголовок текущей записи
 
     def get_absolute_url(self):
         """
         Формирует абсолютный url для шаблона
         :return:
         """
-        return reverse('post', kwargs={'post_slug': self.slug})
+        return reverse('company', kwargs={'slug': self.slug})
 
     class Meta:
-        verbose_name = ''
-        verbose_name_plural = ''
-        ordering = ['time_create', 'title']
+        verbose_name = 'Компания'
+        verbose_name_plural = 'Компании'
+        ordering = ['time_create', 'name']
 
 
-class Product(models.Model):
+class Reviews(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    stars = models.ImageField(blank=True, verbose_name="Звезды")
+    email = models.EmailField(max_length=100, unique=True, verbose_name="Email")
+    company = models.ForeignKey('Company', on_delete=models.PROTECT, verbose_name="Компания")
 
     def __str__(self):
         return self.name
@@ -40,9 +51,9 @@ class Product(models.Model):
         Формирует абсолютный url для шаблона
         :return:
         """
-        return reverse('category', kwargs={'cat_slug': self.slug})
+        return reverse('reviews', kwargs={'slug': self.slug})
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
         ordering = ['id']
--- seoproject/mainapp/urls.py
+++ seoproject/mainapp/urls.py
@@ -3,8 +3,6 @@ from mainapp.views import *
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('about/', About.as_view(), name='about'),
-    path('service/', Service.as_view(), name='service'),
     path('review/', Review.as_view(), name='review'),
     path('contact/', Contact.as_view(), name='contact'),
     ]
