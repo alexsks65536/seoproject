f9fd6d462a39187ffb94bdda1e9262e4c4ed4439
alexsks65536 <megbyte@mail.ru>
2022-12-18 23:39:08 +0300

    Review send mail & BD

--- seoproject/mainapp/urls.py
+++ seoproject/mainapp/urls.py
@@ -5,10 +5,10 @@ from .views import *
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('add_review/', AddReview.as_view(), name='add_review'),
-    path('add_review/success/', SuccessView.as_view(), name='succes'),
+    path('add_review/success/', SuccessView.as_view(), name='success'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
     path('show_company/<slug:company_slug>/', ShowCompany.as_view(), name='show_company'),
     path('contact/', FeedBackView.as_view(), name='contact'),
-    path('contact/success/', SuccessView.as_view(), name='succes'),
+    path('contact/success/', SuccessView.as_view(), name='success'),
 ]
